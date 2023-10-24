f39d2a6e00c1c3fcf54de1488295779bcde3b7bd
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2023-10-24 22:32:52 +0530

    Commit

---
+++ Implementation_Practice/src/ExceptionHandlingPract/Main.java
@@ -0,0 +1,18 @@
+package ExceptionHandlingPract;
+
+public class Main {
+public static void main(String[] args) {
+//        try {
+//            int[] arr = new int[5];
+//            arr[10] = 10;
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("ArrayIndexOutOfBoundsException");
+//        } catch (Exception e) {
+//            System.out.println("Exception");
+//        } finally {
+//            System.out.println("Finally");
+//        }
+
+    System.out.println(1/0);
+    }
+}
